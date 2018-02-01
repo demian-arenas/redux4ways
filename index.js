b99de2c8e088ea3465b232c4dc969d820d0378ad
Demian Arenas <arenasdemian99@gmail.com>
2018-02-01 04:29:29 -0600

    [update] configure index file with Redux Provider

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from "react";
+import { AppRegistry } from "react-native";
 
-AppRegistry.registerComponent('redux4ways', () => App);
+import { Provider } from "react-redux";
+import configureStore from "./configureStore";
+import App from "./app";
+
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent("redux4ways", () => ReduxApp);
