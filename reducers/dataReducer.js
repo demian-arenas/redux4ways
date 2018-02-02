1fa9448691b76cf1a8f25a5571e675ed4aed12ef
Demian Arenas <arenasdemian99@gmail.com>
2018-02-01 18:20:52 -0600

    [update] swap out the actions with the new constants we are working with

--- reducers/dataReducer.js
+++ reducers/dataReducer.js
@@ -1,32 +1,36 @@
-import { FETCHING_DATA, FETCHING_DATA_SUCCESS, FETCHING_DATA_FAILURE } from '../constants'
+import {
+  FETCH_DATA_PENDING,
+  FETCH_DATA_FULFILLED,
+  FETCH_DATA_REJECTED
+} from "../constants";
 const initialState = {
   data: [],
   dataFetched: false,
   isFetching: false,
   error: false
-}
+};
 
-export default function dataReducer (state = initialState, action) {
+export default function dataReducer(state = initialState, action) {
   switch (action.type) {
-    case FETCHING_DATA:
+    case FETCH_DATA_PENDING:
       return {
         ...state,
         data: [],
         isFetching: true
-      }
-    case FETCHING_DATA_SUCCESS:
+      };
+    case FETCH_DATA_FULFILLED:
       return {
         ...state,
         isFetching: false,
-        data: action.data
-      }
-    case FETCHING_DATA_FAILURE:
+        data: action.payload
+      };
+    case FETCH_DATA_REJECTED:
       return {
         ...state,
         isFetching: false,
         error: true
-      }
+      };
     default:
-      return state
+      return state;
   }
 }
