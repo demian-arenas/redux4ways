0e7df2b6b22e8e476469432ccdcf20884cb4042e
Demian Arenas <arenasdemian99@gmail.com>
2018-02-01 18:09:35 -0600

    [update] created action functions to use with Redux observable

--- actions.js
+++ actions.js
@@ -4,7 +4,7 @@ import {
   FETCHING_DATA_FAILURE
 } from "./constants";
 
-export function getData() {
+export function fetchData() {
   return {
     type: FETCHING_DATA
   };
@@ -17,10 +17,9 @@ export function getDataSuccess(data) {
   };
 }
 
-export function getDataFailure() {
+export function getDataFailure(error) {
   return {
-    type: FETCHING_DATA_FAILURE
+    type: FETCHING_DATA_FAILURE,
+    errorMessage: error
   };
 }
-
-export function fetchData() {}
