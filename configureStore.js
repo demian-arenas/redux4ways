3ed8385b25a574a721927f544b413504ae9bae73
Demian Arenas <arenasdemian99@gmail.com>
2018-02-01 18:03:00 -0600

    [update] implement redux saga in configureStore

--- configureStore.js
+++ configureStore.js
@@ -1,7 +1,13 @@
 import { createStore } from "redux";
 import app from "./reducers";
 
+import createSagaMiddleware from "redux-saga";
+import dataSaga from "./saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
 export default function configureStore() {
-  let store = createStore(app);
+  const store = createStore(app, applyMiddleware(sagaMiddleware));
+  sagaMiddleware.run(dataSaga);
   return store;
 }
