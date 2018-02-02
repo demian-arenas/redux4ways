a415a082b1157a130c979ce5db1c8105d3f79399
Demian Arenas <arenasdemian99@gmail.com>
2018-02-01 18:15:32 -0600

    [update] implement epicmiddleware in store and implement Redux-observable in App

--- configureStore.js
+++ configureStore.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import app from "./reducers";
 
+import { createEpicMiddleware } from "redux-observable";
+import fetchUserEpic from "./epic";
+
+const epicMiddleware = createEpicMiddleware(fetchUserEpic);
+
 export default function configureStore() {
-  let store = createStore(app);
+  const store = createStore(app, applyMiddleware(epicMiddleware));
   return store;
 }
